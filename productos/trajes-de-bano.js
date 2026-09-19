e7eea002b1e27c7d604143bcd9dfa224b73e977a
mishellgonzalez1017-art <mishellgonzalez1017@gmail.com>
2026-09-19 13:13:28 -0600

    Eliminación de prendas

--- productos/trajes-de-bano.js
+++ productos/trajes-de-bano.js
@@ -5,7 +5,6 @@ export const trajesDeBano = [
   { id: '#080', img: 'https://i.postimg.cc/wTd9QPFj/20260906-163017.jpg', titulo: 'Traje de baño', precio: 'Q25', talla: 'S/M', marca: 'MARCA', cat: 'Mujer' },
   { id: '#081', img: 'https://i.postimg.cc/pXYRGZ6x/20260906-163132.jpg', titulo: 'Traje de baño', precio: 'Q25', talla: 'S', marca: 'MARCA', cat: 'Mujer' },
   { id: '#082', img: 'https://i.postimg.cc/kXFq1cpP/20260906-163212.jpg', titulo: 'Traje de baño', precio: 'Q30', talla: 'XS', marca: 'VICTORIA SECRET', cat: 'Mujer' },
-  { id: '#083', img: 'https://i.postimg.cc/Hs4dZ2hC/20260906-163450.jpg', titulo: 'Traje de baño NUEVO', precio: 'VENDIDA', talla: 'M', marca: 'MARCA', cat: 'Mujer' },
   { id: '#084', img: 'https://i.postimg.cc/rFSZ5ZYF/20260906-163330.jpg', titulo: 'Traje de baño NUEVO', precio: 'Q30', talla: 'XS/S', marca: 'SHEIN', cat: 'Mujer' },
   { id: '#085', img: 'https://i.postimg.cc/zBBCq42D/IMG-20260809-WA0028.jpg', titulo: 'Traje de baño', precio: 'Q30', talla: 'M', marca: 'MARCA', cat: 'Mujer' },
 ];
